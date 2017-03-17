0990ce5b1f31c96e6dc5543db713edb7c639300d
Clark Hatch <csark@users.noreply.github.com>
2017-03-17 14:46:55 -0700

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -47,7 +47,7 @@ default['nexpose']['last_name'] = 'User'
 default['nexpose']['company_name'] = 'Rapid7'
 # Install type (typical || engine)
 default['nexpose']['component_type'] = 'typical'
-# Communication direction (1 => Engine->Console || 2=> Console->Engine)
+# Communication direction (1 => Engine->Console || 2 => Console->Engine)
 default['nexpose']['communication_direction'] = 1
 # Credentials
 default['nexpose']['username'] = 'nxadmin'
