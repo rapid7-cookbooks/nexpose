8a60bc220fed1501a27e443d9740a46e82703fe8
Alan Sebastian <asebastian2@gmail.com>
2017-05-10 15:08:13 -0700

    Bump version for next development iteration

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan_hass@rapid7.com'
 license          'Apache 2.0'
 description      'Installs/Configures Nexpose'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.6'
+version          '0.3.0'
 
 %w( debian ubuntu redhat windows ).each do |os|
   supports os
